268d46d4c081c9baf089d56f6f1ad15fbe1fcb77
Eric G. Fernandez A. S. <efernandezalbuq@gmail.com>
2024-12-05 12:38:27 -0300

    ajuste secantes

--- secantes.py
+++ secantes.py
@@ -17,26 +17,24 @@ eadm = float(input('>>> '))
 
 print(varx)
 
-# Avaliando os dois primeiros pontos
 for i in range(2):
     x = varx[i]
     fx = eval(funcao[0], {"x": x})
     varfx.append(fx)
-    print(f"{x:10.2f} | f({x:2.2f}) = {varfx[-1]:10.2f}")
+    print(f"{x:10.4f} | f({x:2.4f}) = {varfx[-1]:10.4f}")
 
-# Método das Secantes
 while abs(varx[-1] - varx[-2]) > eadm:
     x = (varfx[-1]*varx[-2] - varfx[-2]*varx[-1]) / (varfx[-1]-varfx[-2])
     # x = varx[-1] - (varfx[-1] * (varx[-1] - varx[-2])) / (varfx[-1] - varfx[-2])
-    fx = eval(funcao[0], {"x": x})
+    fx = eval(funcao[0])
 
     varx.append(x)
     varfx.append(fx)
 
-    print(f"{x:10.2f} | f({x:2.2f}) = {varfx[-1]:10.2f} | Erro = {abs(varx[-1] - varx[-2]):10.4f}")
+    print(f"{x:10.4f} | f({x:2.4f}) = {varfx[-1]:10.4f} | Erro = {abs(varx[-1] - varx[-2]):10.4f}")
 
 # Resultado final
-print(f"Raiz aproximada: {varx[-1]:10.5f} | f(x) = {varfx[-1]:10.5f}")
+print(f"Raiz aproximada: {varx[-1]:10.6f} | f(x) = {varfx[-1]:10.6f}")
 
 
 
